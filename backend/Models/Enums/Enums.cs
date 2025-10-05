cd2b16968a12ac21491678284cffec5ecd234218
AhmadAbd22 <Ahmadbu648@gmail.com>
2025-10-05 20:32:49 +0500

    Added enums.

---
+++ backend/Models/Enums/Enums.cs
@@ -0,0 +1,38 @@
+﻿namespace CourseMate.Models.Enums
+{
+    public enum enumRole
+    {
+    }
+
+    public enum enumMajor
+    {
+        ComputerScience = 1,
+        InformationTechnology = 2,
+        SoftwareEngineering = 3,
+        DataScience = 4,
+        CyberSecurity = 5,
+        NetworkEngineering = 6,
+        ArtificialIntelligence = 7,
+        WebDevelopment = 8,
+        MobileAppDevelopment = 9,
+        CloudComputing = 10,
+    }
+
+    public enum enumYear
+    {
+        FirstYear = 1,
+        SecondYear = 2,
+        ThirdYear = 3,
+        FourthYear = 4,
+        FifthYear = 5,
+    }
+
+    public enum enumSemester
+    {
+        Fall = 1,
+        Spring = 2,
+        Summer = 3,
+    }
+
+
+}
