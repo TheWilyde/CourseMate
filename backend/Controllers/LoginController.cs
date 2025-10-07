7659aa4896fa91bc6f0573639b64d08bbabdd2d7
AhmadAbd22 <Ahmadbu648@gmail.com>
2025-10-07 19:30:34 +0500

    LoginRequestDto added for handling User Login.

---
+++ backend/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using CourseMate.Models.Dtos;
+using CourseMate.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseMate.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        //Dependency Injection
+        private readonly IAuthorizationRepositoryService _authService;
+        public LoginController(IAuthorizationRepositoryService authService)
+        {
+            _authService = authService;
+        }
+        
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            var result = await _authService.Login(loginDto.Email, loginDto.Password);
+            
+            return result;
+
+
+        }
+    }
+}
---
+++ backend/Models/Dtos/LoginRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseMate.Models.Dtos
+{
+    public class LoginRequestDto
+    {
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
