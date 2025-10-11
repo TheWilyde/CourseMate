ab09e79e04cadc71f7cb68e25faf5c406dcc109e
AhmadAbd22 <Ahmadbu648@gmail.com>
2025-10-11 23:47:29 +0500

    Added some DTOs.

---
+++ backend/Models/Dtos/AdminDtoForLogin.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseMate.Models.Dtos
+{
+    public class AdminDtoForLogin
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Role { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ backend/Models/Dtos/SignUpRequestDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseMate.Models.Dtos
+{
+    public class SignUpRequestDto
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public required string LastName { get; set; }
+
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public required string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public required DateTime DateOfBirth { get; set; }
+
+
+    }
+}
