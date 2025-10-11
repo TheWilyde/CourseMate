69f3ae3563083daca564201ca70cdc99400094f8
AhmadAbd22 <Ahmadbu648@gmail.com>
2025-10-11 23:25:00 +0500

    Added helper classes.

---
+++ backend/Models/HelpingClasses/EnumHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseMate.Models.HelpingClasses
+{
+    public static class EnumHelper
+    {
+        public static int ConverEnumToValue<TEnum>(this TEnum enumValue) where TEnum : Enum
+            => Convert.ToInt32(enumValue);
+    }
+
+}
