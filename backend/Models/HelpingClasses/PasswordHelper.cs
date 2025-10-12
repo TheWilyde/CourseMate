dae8ccee7d730a3f4625724a4705b3386fecd225
AhmadAbd22 <Ahmadbu648@gmail.com>
2025-10-12 20:38:02 +0500

    Made a change in PasswordHelper.cs

--- backend/Models/HelpingClasses/PasswordHelper.cs
+++ backend/Models/HelpingClasses/PasswordHelper.cs
@@ -7,9 +7,8 @@ namespace CourseMate.Models.HelpingClasses
     {
         public static string HashPassword(string password)
         {
-            using var sha256 = SHA256.Create();
             var bytes = Encoding.UTF8.GetBytes(password);
-            var hash = sha256.ComputeHash(bytes);
+            var hash = SHA256.HashData(bytes);
             return Convert.ToBase64String(hash);
         }
     }
